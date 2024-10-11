4e656b23c6357d593ae7562f83f411f9eea8fed0
Ashwani <ashwanisingh46572@gmail.com>
2024-10-11 23:53:07 +0530

    check 54

--- src/pages/Your_time.jsx
+++ src/pages/Your_time.jsx
@@ -605,7 +605,7 @@ const IndiaTimeClock = () => {
       setTime(indiaTime);
       setDate(indiaDate);
 
-      if (indiaTime === '11:52:00 PM') {
+      if (indiaTime === '11:54:00 PM') {
         playMidnightAudio();
         triggerConfettiWithDelays();
       }
